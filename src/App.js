03a01b66753d4e6d0391db82bf621f92e8696762
strayder90 <strayder@live.com>
2023-01-29 19:39:41 +0100

    Adds table_reservation app codebase.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from table_reservation!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
